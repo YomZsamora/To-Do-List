5aaff63a78b1b430575ec20b08ac534d8fdcf74d
yomZsamora <samora.y@adzumi.co.ke>
2018-05-17 10:45:58 +0300

    Connecting a Class to the Database (Now Connected).

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,48 +1,54 @@
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import org.sql2o.*;
 
 public class Task {
-  private String mDescription;
-  private boolean mCompleted;
-  private LocalDateTime mCreatedAt;
-  private static List<Task> instances = new ArrayList<Task>();
-  private int mId;
+  private String description;
+  private boolean completed;
+  private LocalDateTime createdAt;
+  private int id;
 
   public Task(String description) {
-    mDescription = description;
-    mCompleted = false;
-    mCreatedAt = LocalDateTime.now();
-    instances.add(this);
-    mId = instances.size();
+    this.description = description;
+    completed = false;
+    createdAt = LocalDateTime.now();
   }
 
   public String getDescription() {
-    return mDescription;
+    return description;
   }
 
   public boolean isCompleted() {
-    return mCompleted;
+    return completed;
   }
 
   public LocalDateTime getCreatedAt() {
-    return mCreatedAt;
-  }
-
-  public static List<Task> all() {
-   return instances;
- }
-
-  public static void clear() {
-    instances.clear();
+    return createdAt;
   }
 
   public int getId() {
-   return mId;
- }
+    return id;
+  }
 
- public static Task find(int id) {
-   return instances.get(id - 1);
- }
+  // public static Task find(int id) {
+  // }
+
+  public static List<Task> all() {
+    String sql = "SELECT id, description FROM tasks";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Task.class);
+    }
+  }
+
+  @Override
+  public boolean equals(Object otherTask) {
+    if (!(otherTask instanceof Task)) {
+      return false;
+    } else {
+      Task newTask = (Task) otherTask;
+      return this.getDescription().equals(newTask.getDescription());
+    }
+  }
 
 }
